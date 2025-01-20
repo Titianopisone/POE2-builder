38424816f338582f178a5c000512ad828af14da2
Titianopisone <titianopisone@gmail.com>
2025-01-20 16:21:07 +0100

    navbar fonctionnel manque css et amelioration de la bbd interne

--- src/components/navebar/navebar.component.ts
+++ src/components/navebar/navebar.component.ts
@@ -10,9 +10,38 @@ import { Classe, ClassesService } from '../../services/classes/classes.service';
   styleUrl: './navebar.component.css'
 })
 export class NavebarComponent {
- classes: Classe[] = []
-  constructor(service: ClassesService) {
-    service.all().subscribe(result => { this.classes = result })
+ classes: Classe[] = [];
+isclassemenuVisible: boolean= false;
+isgemmemenuVisible: boolean= false;
+isequipementmenuVisible: boolean= false;
+isbuildmenuVisible: boolean= false;
+  constructor(private classesService: ClassesService) {
+    this.classesService.all().subscribe(result => {
+      this.classes = result;
+    });
   }
+  toggleSubmenu(submenuname:string): void {
+   this.isbuildmenuVisible=false;
+   this.isclassemenuVisible=false;
+   this.isgemmemenuVisible=false;
+   this.isequipementmenuVisible=false;
+switch(submenuname){
+  case "classe":
+    this.isclassemenuVisible = !this.isclassemenuVisible;
+    break;
+  case "gemme":
+    this.isgemmemenuVisible = !this.isgemmemenuVisible;
+    break;
+  case "equipement":
+    this.isequipementmenuVisible = !this.isequipementmenuVisible;
+    break;
+  case "build":
+    this.isbuildmenuVisible = !this.isbuildmenuVisible;
+    break;
+    
+}  
+  }
+  
   http=inject(HttpClient)
 }
+
